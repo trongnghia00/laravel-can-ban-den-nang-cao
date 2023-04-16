dc37d81ce7c3fb636df87c0ca7c2b7c176995767
Trong Nghia <trongnghia77@live.com>
2023-04-16 09:30:03 +0700

    19-form-custom-validation-text

--- test-project/app/Http/Controllers/LoginController.php
+++ test-project/app/Http/Controllers/LoginController.php
@@ -15,6 +15,10 @@ class LoginController extends Controller
             'username' => ['required', 'alpha_num', 'min:6', 'max:10'],
             'email' => ['required', 'email'],
             'password' => 'required'
+        ], [
+            'username.required' => 'Bạn phải nhập username !!!',
+            'username.alpha_num' => 'Bạn chỉ được sử dụng chữ cái và số cho username',
+            'username.min' => 'Username ít nhất 6 ký tự !!'
         ]);
 
         return $request;
