1fdfb04142fd4b1bcb4805edb306a8aba4c1ca56
Trong Nghia <trongnghia77@live.com>
2023-04-22 00:32:20 +0700

    25-database-retrieve-data

--- test-project/app/Http/Controllers/HomeController.php
+++ test-project/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -14,28 +15,31 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $articles = [
-            [
-                'title' => 'First Article',
-                'content' => 'This is a content',
-                'published' => 1
-            ],
-            [
-                'title' => 'Second Article',
-                'content' => 'This is a content',
-                'published' => 1
-            ],
-            [
-                'title' => 'Third Article',
-                'content' => 'This is a content',
-                'published' => 0
-            ],
-            [
-                'title' => 'Last Article',
-                'content' => 'This is a content',
-                'published' => 1
-            ]
-        ];
+        // $articles = [
+        //     [
+        //         'title' => 'First Article',
+        //         'content' => 'This is a content',
+        //         'published' => 1
+        //     ],
+        //     [
+        //         'title' => 'Second Article',
+        //         'content' => 'This is a content',
+        //         'published' => 1
+        //     ],
+        //     [
+        //         'title' => 'Third Article',
+        //         'content' => 'This is a content',
+        //         'published' => 0
+        //     ],
+        //     [
+        //         'title' => 'Last Article',
+        //         'content' => 'This is a content',
+        //         'published' => 1
+        //     ]
+        // ];
+
+
+        return DB::table('posts')->first();
         return view('home', compact('articles'));
     }
 }
