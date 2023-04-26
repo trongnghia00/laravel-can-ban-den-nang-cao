81c406b15e23e7836054669e85da9879f3561fc0
Trong Nghia <trongnghia77@live.com>
2023-04-27 00:09:34 +0700

    33-database-model-retrieve-data

--- test-project/app/Http/Controllers/HomeController.php
+++ test-project/app/Http/Controllers/HomeController.php
@@ -17,8 +17,18 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        /**
+         * 1. Lấy toàn bộ dữ liệu
+         * 2. Lấy 1 dữ liệu đơn
+         * 3. Lấy 1 dữ liệu hoặc phát sinh lỗi
+         */
+
+        // return $post = MyPost::findOrFail(60);
+
         $posts = MyPost::all();
-        return $posts;
+        foreach ($posts as $post) {
+            echo $post->title . "<br />";
+        }
 
         // return view('home', compact('articles'));
     }
