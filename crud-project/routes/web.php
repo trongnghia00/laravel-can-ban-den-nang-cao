d692bd18461fc7e2f06fae7649836a584e00e817
Trong Nghia <trongnghia77@live.com>
2023-05-20 05:26:20 +0700

    55-crud-template

--- crud-project/routes/web.php
+++ crud-project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('/', PostController::class);
 
 /**
  * CRUD
