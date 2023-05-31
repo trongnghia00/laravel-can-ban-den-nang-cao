b23c73653affb8bb0af909d6e1ee5e50ebec8290
Trong Nghia <trongnghia77@live.com>
2023-05-31 22:20:54 +0700

    61-crud-save-new-post

--- crud-project/app/Http/Controllers/PostController.php
+++ crud-project/app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -43,7 +44,18 @@ class PostController extends Controller
             'description' => ['required']
         ]);
 
-        dd($request->all());
+        $post = new Post();
+
+        $fileName = time() . '_' . $request->image->getClientOriginalName();
+        $filePath = $request->image->storeAs('uploads', $fileName);
+
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->category_id = $request->category_id;
+        $post->image = $filePath;
+        $post->save();
+
+        return redirect()->route('index');
     }
 
     /**
