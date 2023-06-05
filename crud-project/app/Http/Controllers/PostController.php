2d5acf0f15afe89e164bf0342e79be707c068199
Trong Nghia <trongnghia77@live.com>
2023-06-06 06:06:49 +0700

    71-crud-paginate

--- crud-project/app/Http/Controllers/PostController.php
+++ crud-project/app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderByDesc('id')->paginate(4);
 
         return view('index', compact('posts'));
     }
