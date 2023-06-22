0c236ace6fc28e24d3ffaf38c037e3608cabf470
Trong Nghia <trongnghia77@live.com>
2023-06-22 15:14:19 +0700

    83-email-attachment

--- crud-project/app/Mail/TestMail.php
+++ crud-project/app/Mail/TestMail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -31,7 +32,7 @@ class TestMail extends Mailable
     public function envelope()
     {
         return new Envelope(
-            to: '...',
+            to: ['...'],
             subject: 'Test Mail',
         );
     }
@@ -55,6 +56,8 @@ class TestMail extends Mailable
      */
     public function attachments()
     {
-        return [];
+        return [
+            Attachment::fromPath(public_path('/storage/uploads/1686005259_example10.jpg')),
+        ];
     }
 }
