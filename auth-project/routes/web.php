e57121399a58a556091d25bcbe6b8563658b09ab
Trong Nghia <trongnghia77@live.com>
2023-07-14 10:21:20 +0700

    94-auth-userdata

--- auth-project/routes/web.php
+++ auth-project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/user-data', function() {
+    return auth()->user()->email;
+});
