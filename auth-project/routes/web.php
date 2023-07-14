d9d6b56ed555c9e1e3e35321796d7394ac69e1e4
Trong Nghia <trongnghia77@live.com>
2023-07-14 23:26:56 +0700

    96-auth-protect

--- auth-project/routes/web.php
+++ auth-project/routes/web.php
@@ -15,8 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/user-data', function() {
+        return auth()->user()->email;
+    });
 });
 
 Route::get('/dashboard', function () {
@@ -31,6 +37,3 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/user-data', function() {
-    return auth()->user()->email;
-});
