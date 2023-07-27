842c19c74159492bc7248ecdf57535f73f9f2082
Trong Nghia <trongnghia77@live.com>
2023-07-27 23:13:39 +0700

    119-picsProject-format-upload

--- pics_project/resources/views/image/create.blade.php
+++ pics_project/resources/views/image/create.blade.php
@@ -1,23 +1,40 @@
 <x-layout title="Upload new image">
-    <h1>Upload new image</h1>
-
-    <x-form action="{{ route('image.store') }}" method="POST" enctype="multipart/form-data">
-        <div>
-            <label for="file">File</label>
-            <input type="file" name="file" id="file" />
-            @error('file')
-                <div>{{ $message }}</div>
-            @enderror
+    <div class="container-fluid mt-4">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        Upload your image
+                    </div>
+                    <div class="card-body">
+                        <x-form action="{{ route('image.store') }}" method="POST" enctype="multipart/form-data">
+                            <div class="mb-4">
+                                <label class="form-label" for="file">File</label>
+                                <input class="form-control @error('file')
+                                    is-invalid
+                                @enderror" type="file" name="file" id="file" />
+                                @error('file')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-4">
+                                <label class="form-label" for="title">Title</label>
+                                <input class="form-control @error('title')
+                                    is-invalid
+                                @enderror" type="text" name="title" id="title" value="{{ old('title') }}" />
+                                @error('title')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-4">
+                                <button type="submit" class="btn btn-primary">Upload</button>
+                                <a href="{{ route('image.index') }}" class="btn btn-secondary">Cancel</a>
+                            </div>
+                        </x-form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ old('title') }}" />
-            @error('title')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
-        <button type="submit">Upload</button>
-    </x-form>
-
+    </div>
 </x-layout>
 
